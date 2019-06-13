ef8a8632c7cccb71aa991ba482b078326977fea7
Vi Pro <VdustR@gmail.com>
2019-06-13 21:54:28 +0800

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'standard',
+    'plugin:vue/recommended',
+    '@vue/typescript',
+    'prettier',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/html-end-tags': 'off',
+    'vue/html-indent': 'off',
+    'vue/html-quotes': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+};
